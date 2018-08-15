431f38c12459815887aa562d0f757da22c5c331b
salsabeeljaafreh <s.jaafreh@yahoo.com>
2018-08-15 13:28:03 +0300

    retrieve data by the get methode angularjs

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -6,6 +6,13 @@ app.component('cats', {
 app.controller('catsCtrl', function ($scope, $http,$window){
 var getCats = function() {
 	// TODO: Your code here
+	$http({
+		method:'GET',
+		url:'/cats'
+	}).then(function (response){
+			$scope.jsondata = response.data;
+			console.log("status:" + response.status);
+		})
 
 };
 getCats();
